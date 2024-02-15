cf1b1d4fae87f2506473ad0ef78570ddd8aa7875
sangbin-sangbin <sangbin.park@intel.com>
2024-02-16 02:22:57 +0900

    Add files via upload

---
+++ MediaPipe/HandTracker.py
@@ -0,0 +1,407 @@
+import numpy as np
+import cv2
+import os
+import re
+import time
+import openvino.runtime as ov
+from MediaPipe import mediapipe_utils as mpu
+from MediaPipe.FPS import FPS, now
+import tensorflow as tf
+
+class HandTracker:
+    def __init__(
+        self,
+        input_src=None,
+        pd_device="CPU",
+        pd_score_thresh=0.5,
+        pd_nms_thresh=0.3,
+        use_lm=True,
+        lm_device="CPU",
+        lm_score_threshold=0.5,
+        crop=False,
+        is_getdata=False,
+    ):
+
+        self.pd_score_thresh = pd_score_thresh
+        self.pd_nms_thresh = pd_nms_thresh
+        self.use_lm = use_lm
+        self.lm_score_threshold = lm_score_threshold
+        self.crop = crop
+
+        self.is_getdata = is_getdata
+        self.dataset = []
+        self.state = "break"
+        self.start_time = time.time()
+        self.gesture_num = 0
+        self.gestures = ["default", "left", "right", "select", "exit", "shortcut1", "shortcut2", "none"]
+
+        self.recognized = []
+
+        if input_src.endswith(".jpg") or input_src.endswith(".png"):
+            self.image_mode = True
+            self.img = cv2.imread(input_src)
+        else:
+            self.image_mode = False
+            if input_src.isdigit():
+                input_src = int(input_src)
+            # Open a webcam
+            w = 1280
+            h = 720
+            self.cap = cv2.VideoCapture(input_src)
+            self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, w)
+            self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
+
+        # Create SSD anchors
+        # https://github.com/google/mediapipe/blob/master/mediapipe/modules/palm_detection/palm_detection_cpu.pbtxt
+        anchor_options = mpu.SSDAnchorOptions(
+            num_layers=4,
+            min_scale=0.1484375,
+            max_scale=0.75,
+            input_size_height=192,
+            input_size_width=192,
+            anchor_offset_x=0.5,
+            anchor_offset_y=0.5,
+            strides=[8, 16, 16, 16],
+            aspect_ratios=[1.0],
+            reduce_boxes_in_lowest_layer=False,
+            interpolated_scale_aspect_ratio=1.0,
+            fixed_anchor_size=True,
+        )
+        self.anchors = mpu.generate_anchors(anchor_options)
+        self.nb_anchors = self.anchors.shape[0]
+        print(f"{self.nb_anchors} anchors have been created")
+
+        # Load Openvino models
+        self.load_models()
+
+        # Rendering flags
+        if self.use_lm:
+            self.show_pd_box = False
+            self.show_pd_kps = False
+            self.show_rot_rect = False
+            self.show_handedness = False
+            self.show_landmarks = True
+            self.show_scores = False
+        else:
+            self.show_pd_box = True
+            self.show_pd_kps = True
+            self.show_rot_rect = False
+            self.show_scores = False
+
+    # Getter method
+    def __getattribute__(self, name):
+        if name == "dataset":
+            return object.__getattribute__(self, "dataset")
+        else:
+            return object.__getattribute__(self, name)
+
+    # Setter method
+    def __setattr__(self, name, value):
+        if name == "dataset":
+            object.__setattr__(self, "dataset", value)
+        else:
+            object.__setattr__(self, name, value)
+
+    def load_models(self):
+
+        palm_model_path = os.path.join("..", "MediaPipe", "mediapipe_models", "palm_detection_full.tflite")
+
+        self.palm_interpreter = tf.lite.Interpreter(model_path=palm_model_path)
+        self.palm_interpreter.allocate_tensors()
+        
+        # for GPU inference
+        self.palm_interpreter.set_tensor( palm_interpreter.get_delegate().set_gpu_compatibility(True) )
+
+        print(self.palm_interpreter.get_input_details())
+        print(self.palm_interpreter.get_output_details())
+
+        # Input blob: input_1 - shape: [1, 3, 192, 192]
+        # Output blob: Identity - shape: [1, 2016, 18]
+        # Output blob: Identity_1 - shape: [1, 2016, 1]
+        self.pd_input_blob = next(iter(self.palm_interpreter.get_input_details()))["name"]
+        self.pd_input_index = next(iter(self.palm_interpreter.get_input_details()))["index"]
+
+        input_shape = next(iter(self.palm_interpreter.get_input_details()))["shape"]
+        print(f"Input blob: {self.pd_input_blob} - shape: {input_shape}")
+        _, self.pd_w, self.pd_h, _ = (next(iter(self.palm_interpreter.get_input_details()))["shape"])
+
+        self.pd_output_index_1 = self.palm_interpreter.get_output_details()[1]["index"]
+        self.pd_output_index_2 = self.palm_interpreter.get_output_details()[0]["index"]
+
+        self.pd_infer_time_cumul = 0
+        self.pd_infer_nb = 0
+
+        self.infer_nb = 0
+        self.infer_time_cumul = 0
+    
+        self.pd_scores = "Identity_1"
+        self.pd_bboxes = "Identity"
+
+        # Landmarks model
+        if self.use_lm:
+            landmark_model_path = os.path.join("..", "MediaPipe", "mediapipe_models", "hand_landmark_full.tflite")
+
+            self.landmark_interpreter = tf.lite.Interpreter(model_path=landmark_model_path)
+            self.landmark_interpreter.allocate_tensors()
+
+            # for GPU inference
+            self.landmark_interpreter.set_tensor( landmark_interpreter.get_delegate().set_gpu_compatibility(True) )
+
+            print(self.landmark_interpreter.get_input_details())
+            print(self.landmark_interpreter.get_output_details())
+
+            # Input blob: input_1 - shape: [1, 3, 224, 224]
+            # Output blob: Identity_1 - shape: [1, 1]
+            # Output blob: Identity_2 - shape: [1, 1]
+            # Output blob: Identity_3_dense/BiasAdd/Add - shape: [1, 63]
+            # Output blob: Identity_dense/BiasAdd/Add - shape: [1, 63]
+            self.lm_input_blob = next(iter(self.landmark_interpreter.get_input_details()))["name"]
+            self.lm_input_index = next(iter(self.landmark_interpreter.get_input_details()))["index"]
+            input_shape = next(iter(self.landmark_interpreter.get_input_details()))["shape"]
+            print(f"Input blob: {self.lm_input_blob} - shape: {input_shape}")
+            
+            _, self.lm_w, self.lm_h, _ = (next(iter(self.landmark_interpreter.get_input_details()))["shape"])
+
+            self.lm_output_index_1 = self.landmark_interpreter.get_output_details()[1]["index"]
+            self.lm_output_index_2 = self.landmark_interpreter.get_output_details()[2]["index"]
+            self.lm_output_index_3 = self.landmark_interpreter.get_output_details()[0]["index"]
+
+            self.lm_score = "Identity_1"
+            self.lm_handedness = "Identity_2"
+            self.lm_landmarks = "Identity_dense/BiasAdd/Add"
+            self.lm_infer_time_cumul = 0
+            self.lm_infer_nb = 0
+            self.lm_hand_nb = 0
+
+    def pd_postprocess(self, inference):
+        scores = np.squeeze(inference[self.pd_scores])  # 2016
+        bboxes = inference[self.pd_bboxes][0]  # 2016x18
+        # Decode bboxes
+        self.regions = mpu.decode_bboxes(self.pd_score_thresh, scores, bboxes, self.anchors)
+        # Non maximum suppression
+        self.regions = mpu.non_max_suppression(self.regions, self.pd_nms_thresh)
+        if self.use_lm:
+            mpu.detections_to_rect(self.regions)
+            mpu.rect_transformation(self.regions, self.frame_size, self.frame_size)
+
+    def pd_render(self, frame):
+        for r in self.regions:
+            if self.show_pd_box:
+                box = (np.array(r.pd_box) * self.frame_size).astype(int)
+                cv2.rectangle(frame, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]), (0, 255, 0), 2)
+            if self.show_pd_kps:
+                for i, kp in enumerate(r.pd_kps):
+                    x = int(kp[0] * self.frame_size)
+                    y = int(kp[1] * self.frame_size)
+                    cv2.circle(frame, (x, y), 6, (0, 0, 255), -1)
+                    cv2.putText(frame, str(i), (x, y + 12), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+            if self.show_scores:
+                cv2.putText(
+                    frame,
+                    f"Palm score: {r.pd_score:.2f}",
+                    (int(r.pd_box[0] * self.frame_size + 10), int((r.pd_box[1] + r.pd_box[3]) * self.frame_size + 60)),
+                    cv2.FONT_HERSHEY_PLAIN,
+                    2,
+                    (255, 255, 0),
+                    2,
+                )
+
+    def lm_postprocess(self, region, inference):
+        region.lm_score = np.squeeze(inference[self.lm_score])
+        region.handedness = np.squeeze(inference[self.lm_handedness])
+        lm_raw = np.squeeze(inference[self.lm_landmarks])
+
+        lm = []
+        for i in range(int(len(lm_raw) / 3)):
+            # x,y,z -> x/w,y/h,z/w (here h=w)
+            lm.append(lm_raw[3 * i : 3 * (i + 1)] / self.lm_w)
+        region.landmarks = lm
+        self.lanmark_list = [[float(coords[0]), float(coords[1])] for coords in region.landmarks]
+        # self.recognized.append(self.lanmark_list)
+
+    def lm_render(self, frame, region):
+        if region.lm_score > self.lm_score_threshold:
+            if self.is_getdata:
+                # Store the lnadmarks in dataset when recording
+                if self.state == "recording":
+                    self.dataset.append(
+                        {"landmarks": mpu.normalize_points(self.lanmark_list), "gesture": self.gesture_num}
+                    )
+
+                if (time.time() - self.start_time) > 30 and self.state == "recording":
+                    self.start_time = time.time()
+                    self.state = "break"
+                    self.gesture_num = (self.gesture_num + 1) % len(self.gestures)
+                elif (time.time() - self.start_time) > 10 and self.state == "break":
+                    self.start_time = time.time()
+                    self.state = "recording"
+
+                if self.state == "break":
+                    cv2.putText(
+                        frame,
+                        'Break.. Next gesture is "' + self.gestures[self.gesture_num] + '"',
+                        (frame.shape[1] // 2 - 430, frame.shape[0] // 2 - 220),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        2,
+                        (255, 0, 0),
+                        3,
+                    )
+                else:
+                    cv2.putText(
+                        frame,
+                        'Recorging "' + self.gestures[self.gesture_num] + '"',
+                        (frame.shape[1] // 2 - 430, frame.shape[0] // 2 - 220),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        2,
+                        (255, 0, 0),
+                        3,
+                    )
+            else:
+                self.dataset.append({"landmarks": mpu.normalize_points(self.lanmark_list), "gesture": self.gesture_num})
+
+            if self.show_rot_rect:
+                cv2.polylines(frame, [np.array(region.rect_points)], True, (0, 255, 255), 2, cv2.LINE_AA)
+
+            if self.show_landmarks:
+                self.landmarks = []
+
+                src = np.array([(0, 0), (1, 0), (1, 1)], dtype=np.float32)
+                dst = np.array(
+                    [(x, y) for x, y in region.rect_points[1:]], dtype=np.float32
+                )  # region.rect_points[0] is left bottom point !
+                mat = cv2.getAffineTransform(src, dst)
+                lm_xy = np.expand_dims(np.array([(l[0], l[1]) for l in region.landmarks]), axis=0)
+                lm_xy = np.squeeze(cv2.transform(lm_xy, mat)).astype(np.int32)
+                list_connections = [
+                    [0, 1, 2, 3, 4],
+                    [0, 5, 6, 7, 8],
+                    [5, 9, 10, 11, 12],
+                    [9, 13, 14, 15, 16],
+                    [13, 17],
+                    [0, 17, 18, 19, 20],
+                ]
+                lines = [np.array([lm_xy[point] for point in line]) for line in list_connections]
+                cv2.polylines(frame, lines, False, (255, 0, 0), 2, cv2.LINE_AA)
+                self.landmarks.append(lm_xy)
+                if self.is_getdata:
+                    for x, y in lm_xy:
+                        cv2.circle(frame, (x, y), 6, (0, 128, 255), -1)
+
+            if self.show_handedness:
+                cv2.putText(
+                    frame,
+                    f"RIGHT {region.handedness:.2f}" if region.handedness > 0.5 else f"LEFT {1-region.handedness:.2f}",
+                    (
+                        int(region.pd_box[0] * self.frame_size + 10),
+                        int((region.pd_box[1] + region.pd_box[3]) * self.frame_size + 20),
+                    ),
+                    cv2.FONT_HERSHEY_PLAIN,
+                    2,
+                    (0, 255, 0) if region.handedness > 0.5 else (0, 0, 255),
+                    2,
+                )
+            if self.show_scores:
+                cv2.putText(
+                    frame,
+                    f"Landmark score: {region.lm_score:.2f}",
+                    (
+                        int(region.pd_box[0] * self.frame_size + 10),
+                        int((region.pd_box[1] + region.pd_box[3]) * self.frame_size + 90),
+                    ),
+                    cv2.FONT_HERSHEY_PLAIN,
+                    2,
+                    (255, 255, 0),
+                    2,
+                )
+
+    def run(self):
+
+        self.fps = FPS(mean_nb_frames=20)
+
+        nb_pd_inferences = 0
+        nb_lm_inferences = 0
+        glob_pd_rtrip_time = 0
+        glob_lm_rtrip_time = 0
+        while True:
+            self.fps.update()
+            if self.image_mode:
+                vid_frame = self.img
+            else:
+                ok, vid_frame = self.cap.read()
+                if not ok:
+                    break
+            h, w = vid_frame.shape[:2]
+            if self.crop:
+                # Cropping the long side to get a square shape
+                self.frame_size = min(h, w)
+                dx = (w - self.frame_size) // 2
+                dy = (h - self.frame_size) // 2
+                video_frame = vid_frame[dy : dy + self.frame_size, dx : dx + self.frame_size]
+            else:
+                # Padding on the small side to get a square shape
+                self.frame_size = max(h, w)
+                pad_h = int((self.frame_size - h) / 2)
+                pad_w = int((self.frame_size - w) / 2)
+                video_frame = cv2.copyMakeBorder(vid_frame, pad_h, pad_h, pad_w, pad_w, cv2.BORDER_CONSTANT)
+
+            # Resize image to NN square input shape
+            frame_nn = cv2.resize(video_frame, (self.pd_w, self.pd_h), interpolation=cv2.INTER_AREA)
+
+            # Transpose hxwx3 -> 1x3xhxw
+            frame_nn = np.transpose(frame_nn, (2, 0, 1))[None,]
+
+            annotated_frame = video_frame.copy()
+
+            self.palm_interpreter.set_tensor(self.pd_input_index, frame_nn)
+            self.palm_interpreter.invoke()
+            inference = self.palm_interpreter.get_tensor(self.pd_output_index)
+            
+            self.pd_postprocess(inference)
+            self.pd_render(annotated_frame)
+
+            # Hand landmarks
+            if self.use_lm:
+                for i, r in enumerate(self.regions):
+                    frame_nn = mpu.warp_rect_img(r.rect_points, video_frame, self.lm_w, self.lm_h)
+                    # Transpose hxwx3 -> 1x3xhxw
+                    frame_nn = np.transpose(frame_nn, (2, 0, 1))[None,]
+
+                    self.landmark_interpreter.set_tensor(self.lm_input_index, frame_nn)
+                    self.landmark_interpreter.invoke()
+                    inference = self.landmark_interpreter.get_tensor(self.lm_output_index)
+                            
+                    self.lm_postprocess(r, inference)
+                    self.lm_render(annotated_frame, r)
+
+            if not self.crop:
+                annotated_frame = annotated_frame[pad_h : pad_h + h, pad_w : pad_w + w]
+
+            self.fps.display(annotated_frame, orig=(50, 50), color=(240, 180, 100))
+            cv2.imshow("video", annotated_frame)
+
+            key = cv2.waitKey(1)
+            if key == ord("q") or key == 27:
+                break
+            elif key == 32:
+                # Pause on space bar
+                cv2.waitKey(0)
+            elif key == ord("1"):
+                self.show_pd_box = not self.show_pd_box
+            elif key == ord("2"):
+                self.show_pd_kps = not self.show_pd_kps
+            elif key == ord("3"):
+                self.show_rot_rect = not self.show_rot_rect
+            elif key == ord("4"):
+                self.show_landmarks = not self.show_landmarks
+            elif key == ord("5"):
+                self.show_handedness = not self.show_handedness
+            elif key == ord("6"):
+                self.show_scores = not self.show_scores
+            elif key == ord("7"):
+                self.show_gesture = not self.show_gesture
+
+        # Print some stats
+        print(f"# palm detection inferences : {nb_pd_inferences}")
+        print(f"# hand landmark inferences  : {nb_lm_inferences}")
+        print(f"Palm detection round trip   : {glob_pd_rtrip_time/nb_pd_inferences*1000:.1f} ms")
+        print(f"Hand landmark round trip    : {glob_lm_rtrip_time/nb_lm_inferences*1000:.1f} ms")
---
+++ test/test.py
@@ -0,0 +1,103 @@
+import cv2
+import torch
+import torch.nn as nn
+import json
+
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from MediaPipe.HandTracker import HandTracker
+import test_utils as utils
+from run import run
+
+
+class Model(nn.Module):
+    def __init__(self, input_size, hidden_dim1, hidden_dim2, target_size):
+        super(Model, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_dim1)
+        self.fc2 = nn.Linear(hidden_dim1, hidden_dim2)
+        self.fc3 = nn.Linear(hidden_dim2, target_size)
+        self.softmax = nn.Softmax(dim=0)
+
+    def forward(self, landmarks):
+        x = self.fc1(landmarks)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        res = self.softmax(x)
+        return res
+
+
+def initialize_model():
+    INPUT_SIZE = 42
+    HIDDEN_DIM1 = 32
+    HIDDEN_DIM2 = 32
+    TARGET_SIZE = 8
+
+    model = Model(INPUT_SIZE, HIDDEN_DIM1, HIDDEN_DIM2, TARGET_SIZE)
+    model.load_state_dict(torch.load("../model.pt"))
+
+    return model
+
+
+def load_parameters(parameters_dir):
+    res = "n" # input("want to use saved parameters? [ y / n ]\n>>> ")
+    if res == "y":
+        parameter = json.load(open(parameters_dir))
+    else:
+        parameter = {
+            "time": 10,
+            "same_hand": 10,
+            "skip_frame": 1,
+            "start_time": 1,
+            "stop_time": 1,
+            "multi_time": 1,
+            "multi_cooltime": 2,
+        }
+
+    return parameter
+
+
+def create_trackbars(parameter):
+    cv2.namedWindow("gesture recognition")
+    cv2.createTrackbar("time", "gesture recognition", parameter["time"], 100, utils.nothing)
+    cv2.createTrackbar("same_hand", "gesture recognition", parameter["same_hand"], 100, utils.nothing)
+    cv2.createTrackbar("skip_frame", "gesture recognition", parameter["skip_frame"], 50, utils.nothing)
+    cv2.createTrackbar("start_time", "gesture recognition", parameter["start_time"], 10, utils.nothing)
+    cv2.createTrackbar("stop_time", "gesture recognition", parameter["stop_time"], 10, utils.nothing)
+    cv2.createTrackbar("multi_time", "gesture recognition", parameter["multi_time"], 10, utils.nothing)
+    cv2.createTrackbar("multi_cooltime", "gesture recognition", parameter["multi_cooltime"], 10, utils.nothing)
+
+
+def save_current_parameters(parameters_dir):
+    res = input("want to save current parameters? [ y / n ]\n>>> ")
+    if res == "y":
+        with open(parameters_dir, "w") as f:
+            json.dump(parameter, f)
+
+
+if __name__ == "__main__":
+    ht = HandTracker(
+        input_src="0",
+        pd_device="GPU",
+        pd_score_thresh=0.6,
+        pd_nms_thresh=0.3,
+        use_lm=True,
+        lm_device="GPU",
+        lm_score_threshold=0.6,
+        crop=False,
+        is_getdata=False,
+    )
+
+    model = initialize_model()
+
+    parameters_dir = "./parameters.json"
+    parameter = load_parameters(parameters_dir)
+    create_trackbars(parameter)
+
+    run(ht, model)
+
+    # Release the webcam and close all windows
+    ht.cap.release()
+    cv2.destroyAllWindows()
+
+    save_current_parameters(parameters_dir)
